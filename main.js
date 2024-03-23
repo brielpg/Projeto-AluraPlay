63761637abe981d8cfdc329b4b3e8031d0fc469c
Gabriel <144123550+brielpg@users.noreply.github.com>
2024-03-23 16:15:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const moonButton = document.querySelector(".cabecalho__moon");
-   
-    const moonImage = "./img/cabecalho/moon.png";
-    const sunImage = "./img/cabecalho/sun.png";
-
-    let isDarkMode = false;
 
     moonButton.addEventListener("click", function() {
         toggleDarkMode();
@@ -13,13 +8,5 @@ document.addEventListener("DOMContentLoaded", function() {
     function toggleDarkMode() {
         const body = document.querySelector("body");
         body.classList.toggle("dark-mode");
-
-        const image = moonButton.querySelector("img");
-        if (isDarkMode) {
-            image.src = moonImage;
-        } else {
-            image.src = sunImage;
-        }
-        isDarkMode = !isDarkMode;
     }
 });
